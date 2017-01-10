bc0da45746bb9f613f489cc7b554ad5f37351040
kalyanimantha15 <kalyani.m@motivitylabs.com>
2017-01-10 18:44:59 +0530

    Add files via upload

---
+++ python_booleans.py
@@ -0,0 +1,9 @@
+print "Python has 10 type of booleans", True, " and ", False
+print "They are normally the result of comparisons"
+
+num = 8
+if num < 10:
+    print num, " is less than 10"
+    print num, "extra stmt"
+else:
+    print num, " is not less than 10"
